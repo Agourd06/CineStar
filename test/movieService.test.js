3b4ecb0e2e48bf5661f9772a7be7db3db16fba54
Agourd06 <oualidagourd@gmail.com>
2024-09-26 16:08:25 +0100

    Feat: add unitTest for my get all movies function

--- test/movieService.test.js
+++ test/movieService.test.js
@@ -51,7 +51,34 @@ describe('MovieService', () => {
             await expect(createMovie(mockMovieData)).rejects.toThrow('movie is not added : ' + errorMessage);
         });
     });
+    describe('getAllMovies', () => {
+        it('should return a list of movies if found', async () => {
+            const mockMovies = [{
+                _id: 'mockMovieId1',
+                name: 'Movie 1',
+            }, {
+                _id: 'mockMovieId2',
+                name: 'Movie 2',
+            }];
+            Movie.find.mockResolvedValue(mockMovies);
 
+            const result = await getAllMovies();
+
+            expect(Movie.find).toHaveBeenCalledWith({
+                deleted_at: null
+            });
+            expect(result).toEqual(mockMovies);
+        });
+
+        it('should throw an error if fetching movies fails', async () => {
+            const errorMessage = 'Database error';
+            Movie.find.mockRejectedValue(new Error(errorMessage));
+
+            await expect(getAllMovies()).rejects.toThrow('Error fetching movies: ' + errorMessage);
+        });
+    });
+
+  
 
 
 });
